fa90472b5ae78b58db4bc16e2de651f16539188f
Your NameCamusSisyphus <masonwu624@gmail.com>
2024-10-01 17:55:27 -0400

    add cell class and add default line color as black

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from graphics import *
+from cell import Cell
 
 def main():
     win = Window(800,600)
-    p1 = Point(0,0)
-    p2 = Point(800,600)
-    line = Line(p1,p2)
-    win.draw_line(line, "red")
+    cell1 = Cell(win)
+    cell1.draw(50,50,100,100)
     win.wait_for_close()
 
 
